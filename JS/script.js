5c0d11036f9c49f231a1abf108e6f12819096f5d
Alexander <39833630+Alexander1357@users.noreply.github.com>
2018-06-15 19:52:51 +0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -2,14 +2,15 @@
 function fromSysToSys(){
             
             const alph = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
-            var n = document.getElementById('key').value;
+            var input = document.getElementById('key').value;
             var r1 = +document.getElementById('sys1').value;
             var r2 = +document.getElementById('sys2').value;
-            var str;
+            var str = 0;
+            var ans = "";
             document.getElementById('ans').innerHTML = "";
             
-            /*if (r1 != 10){
-                        n.split("").reverse();
+            if (r1 != 10){
+                        n = input.split("").reverse();
                         for (var i = 0; i < n.length; i++){
                                     for (var j = 0; j < alph.length; j++){
                                                 if (n[i] == alph[j]){
@@ -20,14 +21,16 @@ function fromSysToSys(){
                         }
                         
                         for (var i = 0; i < n.length; i++){
-                                    str += +n[i] * Math.pow(r1, i);
+                                    n[i] = parseInt(n[i], 10);
                         }
+                        
+                        for (var i = 0; i < n.length; i++){
+                                    str += n[i] * Math.pow(r1, i);
+                        }  
             }
             
-            n.join("") = "";
-            
             while (str > 0) {
-                +n += alph[str % r2];
+                ans += alph[str % r2];
                 str = str / r2;
                 if (str < Math.round(str)) {
                     str = Math.round(str);
@@ -35,7 +38,7 @@ function fromSysToSys(){
                 }
                 else
                     str = Math.round(str);
-            }*/
+            }
             
             document.getElementById('ans').innerHTML += n.split("").reverse().join("");
 }
