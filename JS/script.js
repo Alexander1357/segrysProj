6d801d037dbf9abdf00d047caa6ccd51b9bf16f2
Alexander <39833630+Alexander1357@users.noreply.github.com>
2018-06-15 18:41:04 +0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,39 +1,41 @@
 
-const alph = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
-
-function fromSysToSyst(string str){
-            var n = str.split("");
-            var r = +document.getElementById('syst').value;
-            for (var i = 0; i < n.length; i++){
-                        for (var j = 0; j < alph.length; j++){
-                                    if (n[i] == alph[j]){
-                                                n[i] = j;
-                                                j = alph.length;
+function fromSysToSyst(){
+            
+            const alph = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+            var n = document.getElementById('key').value;
+            var r1 = +document.getElementById('sys1').value;
+            var r2 = +document.getElementById('sys2').value;
+            var str = "";
+            document.getElementById('ans').innerHTML = "";
+            
+            if (r1 != 10){
+                        n.split("").reverse();
+                        for (var i = 0; i < n.length; i++){
+                                    for (var j = 0; j < alph.length; j++){
+                                                if (n[i] == alph[j]){
+                                                            n[i] = j;
+                                                            j = alph.length;
+                                                }
                                     }
                         }
+                        
+                        for (var i = 0; i < n.length; i++){
+                                    str += +n[i] * Math.pow(r1, i);
+                        }
             }
-            for (var i = 0; i < n.length; i++){
-                        str += +n[i] * Math.pow(r, i);
-            }
-            return str;
-}
-
-function fromDecToSys() {
-    
-            var n = +document.getElementById('key').value;
-            var r = +document.getElementById('sys').value;
-            var s = "";
-            document.getElementById('ans').innerHTML = "";
-            while (n > 0) {
-                s += alph[n % r];
-                n = n / r;
-                if (n < Math.round(n)) {
-                    n = Math.round(n);
-                    n--;
+            
+            n.join("") = "";
+            
+            while (str > 0) {
+                +n += alph[str % r2];
+                str = str / r2;
+                if (str < Math.round(str)) {
+                    str = Math.round(str);
+                    str--;
                 }
                 else
-                    n = Math.round(n);
+                    str = Math.round(str);
             }
-            s = fromSysToSyst(s);
-    document.getElementById('ans').innerHTML += s.split("").reverse().join("");
+            
+            document.getElementById('ans').innerHTML += str.split("").reverse().join("");
 }
