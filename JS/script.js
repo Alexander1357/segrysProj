0593a178270f70d5398e7225a92dde901f549325
Alexander <39833630+Alexander1357@users.noreply.github.com>
2018-06-15 19:00:55 +0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
 
-function fromSys1ToSys2(){
+function fromSysToSys(){
             
             const alph = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
             var n = document.getElementById('key').value;
