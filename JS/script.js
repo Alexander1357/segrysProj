5de72cd9a38d1cd4e92c3693e2d52d6b00a25325
Alexander <39833630+Alexander1357@users.noreply.github.com>
2018-06-19 17:16:02 +0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -23,11 +23,7 @@ function fromSysToSys(){
                         }
                         
                         for (var i = 0; i < n.length; i++){
-                                    n[i] = parseInt(n[i], 10);
-                        }
-                        
-                        for (var i = 0; i < n.length; i++){
-                                    str += n[i] * Math.pow(r1, i);
+                                    str += parseInt(n[i], 10) * Math.pow(r1, i);
                         }  
             }
             
