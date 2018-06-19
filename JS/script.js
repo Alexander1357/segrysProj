60ce61c7b9a9c1a518879becb6a0eff807ed02eb
Alexander <39833630+Alexander1357@users.noreply.github.com>
2018-06-19 17:30:40 +0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@
 function fromSysToSys(){
             
             const alph = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", 
-                          "E", "F", "G", "H", "I" "J", "K", "L", "M", "N", "O", "P", "Q" "R",
+                          "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
                           "S", "T", "U", "V", "W", "X", "Y", "Z"];
             var input = document.getElementById('key').value;
             var r1 = +document.getElementById('sys1').value;
