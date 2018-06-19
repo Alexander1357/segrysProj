d79e690d0e320d0d4357a2415dcf633b3a012ee5
Alexander <39833630+Alexander1357@users.noreply.github.com>
2018-06-19 19:38:21 +0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
 
-function fromSysToSys(){
+function fromSysToSys () {
             
             const alph = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", 
                           "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
@@ -24,7 +24,7 @@ function fromSysToSys(){
             } 
             
             for(let i = 0; i < n.length; i++){
-                        if(n[i] >= r1 || +input < 0){
+                        if(n[i] >= r1 || parseInt(n.join(''), 10) < 0){
                                    str = -1;
                            }
             }
