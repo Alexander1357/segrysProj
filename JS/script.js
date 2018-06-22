a17b3c6b0461dd7d7e43de8ce4f23682db0019db
Alexander <39833630+Alexander1357@users.noreply.github.com>
2018-06-22 15:33:26 +0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -64,7 +64,7 @@ function fromSysToSys () {
                         }
                         
                         //вывод ответа в виде ИСХОДНОЕ ЧИСЛО исходная с.с. = РЕЗУЛЬТАТ нужная с.с.
-                        if (r1 != 0 || r2 != 0 || parseInt(input, 10) != 0){
+                        if (r1 != 0 && r2 != 0 && parseInt(input, 10) != 0){
                                     document.getElementById('ans').innerHTML += `${input}<sub>${String(r1)}</sub> = ${ans.split('').reverse().join('')}<sub>${String(r2)}</sub>`;
                         }
                         else{
