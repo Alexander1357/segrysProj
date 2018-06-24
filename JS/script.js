af37d86dc435e17ecd34dcaa4ec3e9e648087d60
Alexander <39833630+Alexander1357@users.noreply.github.com>
2018-06-24 20:45:40 +0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
-function cleanUp () {
+function cleanUp () {    //очистка полей ввода и вывода ответа
             
             document.getElementById('key').value = '';   
             document.getElementById('ans').innerHTML = ''; 
@@ -16,14 +16,14 @@ function fromSysToSys () {
             let input = document.getElementById('key').value;    //исходное число
             let str = 0;    //вспомогательная переменная
             let ans = "";    //переменная, в которую будет записан ответ
-            let x = 0;
+            let x = 0;    //вспомогательная переменная
             document.getElementById('ans').innerHTML = '';    //чистим строку с ответом
             
             n = input.split('').reverse();    //обЪявляем переменную, в которую записывается массив из цифр исходного числа и переворачивается
             
             for (let i = 0; i < n.length; i++){    //прогоняем массив через два цикла, чтобы заменить буквы на цифры
                         for (let j = 0; j < alph.length; j++){
-                                    if (n[i] == '.'){
+                                    if (n[i] == '.'){    //если число дробное, то запоминаем позицию точки и убираем
                                                 x = -i;
                                                 n.splice(i, 1);
                                     }
@@ -38,7 +38,7 @@ function fromSysToSys () {
             
             
             for(let i = 0; i < n.length; i++){    //прогоняем результат через цикл, чтобы проверить правильность ввода
-                        if(n[i] >= r1 || isNaN(r1) || isNaN(r2) || r1 < 2 || r1 > 36 || r2 < 2 || r2 > 36 || n.length > 25){    //проверяем правильность ввода данных
+                        if(n[i] >= r1 || n.length > 25){    //проверяем правильность ввода данных
                                    str = -1;    //если что-то введено неправильно, то вспомогательная переменная меняется на -1, пропускается выполнения перевода 
                            }
             }
@@ -54,23 +54,23 @@ function fromSysToSys () {
                         
                         //Перевод из десятичной системы счисления в нужную
                         
-                        var h = str;
-                        str = Math.floor(str);
-                        
-                        if (h != Math.floor(h)){
-                                    let f1 =  h - Math.floor(h);
-                                    while (Math.floor(f1) != f1 ){
-                                                f1 *= r2;
-                                                ans += Math.floor(f1);
-                                                f1 -= Math.floor(f1);
-                                                if (ans.length == 15){
-                                                            f1 = Math.floor(f1);
+                        if (h != Math.floor(h)){    //проверка числа на наличие дробной части
+                                    let h = str;    //запоминаем результат для перевода дробной части  
+                                    let f1 =  h - Math.floor(h);    //берём дробную часть из числа
+                                    while (Math.floor(f1) != f1 ){    //запускаем цикл, пока дробная часть не обнулится
+                                                f1 *= r2;    //умножаем дробную часть на основание нужной системы счисления
+                                                ans += Math.floor(f1);    //записываем в результат целое число
+                                                f1 -= Math.floor(f1);    //делаем из числа дробное число с целой частью 0
+                                                if (ans.length == 15){    //если длина дроби больше 15, то заканчиваем перевод
+                                                            f1 = Math.floor(f1);    //заканчиваем перевод
                                                 }
                                     }
-                                    n = ans.split('').reverse().join('');
-                                    ans = n;
-                                    ans += '.';
+                                    n = ans.split('').reverse().join('');    //в свободную переменную записываем результат перевода 
+                                    ans = n;    //записываем результат в ответ
+                                    ans += '.';    //отделяем точкой
                         } 
+                        
+                        str = Math.floor(str);    //переводим целую часть
                         
                         while (str > 0) {    //запускаем цикл перевода
                             ans += alph[str % r2];    //остаток от деления записываем в ответ
@@ -78,10 +78,10 @@ function fromSysToSys () {
                         }
                         
                         //вывод ответа в виде ИСХОДНОЕ ЧИСЛО исходная с.с. = РЕЗУЛЬТАТ нужная с.с.
-                        if (r1 != 0 && r2 != 0 && parseInt(input, 10) > 0){
+                        if (parseInt(input, 10) > 0){    //если поле с исходным числом заполнено, то выводим ответ
                                     document.getElementById('ans').innerHTML += `${input}<sub>${String(r1)}</sub> = ${ans.split('').reverse().join('')}<sub>${String(r2)}</sub>`;
                         }
-                        else{
+                        else{    //иначе выводим сообщение об ошибке
                                     document.getElementById('ans').innerHTML += 'Обязательное поле не заполнено!';
                         }
             }
